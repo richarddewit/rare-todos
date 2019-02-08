8fc58f875c85005c65d75d023bd98f2fcec23d4e
Richard de Wit <henk.exe@gmail.com>
2019-02-09 00:09:40 +0100

    Rename app to RaRe TODOs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "todo_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "rare_todos_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
