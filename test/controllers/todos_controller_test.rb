d9e19c710db828be22cd0214ed10f1a3128c5641
Richard de Wit <henk.exe@gmail.com>
2019-02-10 00:02:24 +0100

    Extract repeated code to their own methods and add delete test
    Achieved 100% test coverage! (for now)

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -3,17 +3,26 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  test "index should return a JSON list of Todos" do
+  def retrieve_list
     get todos_path
     assert_response :success
+    JSON.parse @response.body
+  end
 
-    todos = JSON.parse @response.body
+  def retrieve_single(id)
+    get todo_path id: id
+    assert_response :success
+    JSON.parse @response.body
+  end
+
+  test "index should return a JSON list of Todos" do
+    todos = retrieve_list
     assert todos.is_a? Array
     assert todos[0].is_a? Object
     assert todos[0]["title"].is_a? String
   end
 
-  test "creating one should be valid" do
+  test "creating a todo should be valid" do
     post todos_path, as: :json, params: { title: "New todo" }
     assert_response :created
 
@@ -21,49 +30,42 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
   end
 
-  test "creating one should increase the index count" do
+  test "creating a todo should increase the index count" do
     # Retrieve initial list
-    get todos_path
-    assert_response :success
-    todos = JSON.parse @response.body
-    assert_equal todos.count, 20
+    todos = retrieve_list
+    initial_count = todos.count
 
-    # Add one
+    # Add a todo
     post todos_path, as: :json, params: { title: "New todo" }
     assert_response :created
 
     # Retrieve new list
-    get todos_path
-    assert_response :success
-    todos = JSON.parse @response.body
-    assert_equal todos.count, 21
+    todos = retrieve_list
+    assert_equal todos.count, initial_count + 1
   end
 
-  test "show should return one todo" do
-    get todos_path
-    assert_response :success
-    todos = JSON.parse @response.body
+  test "show should return a todo" do
+    todos = retrieve_list
     first_id = todos[0]["id"]
 
-    get todo_path id: first_id
-    assert_response :success
-    todo = JSON.parse @response.body
+    todo = retrieve_single first_id
     assert todo.is_a? Object
     assert todo["title"].is_a? String
   end
 
-  test "update should alter one todo" do
-    # Get all todos and the first one's ID
-    get todos_path
-    assert_response :success
-    todos = JSON.parse @response.body
+  test "update should alter a todo" do
+    # Get all todos and the first todo's ID
+    todos = retrieve_list
     first_id = todos[0]["id"]
 
     # Get the todo and store initial title
-    get todo_path id: first_id
-    assert_response :success
-    todo = JSON.parse @response.body
+    todo = retrieve_single first_id
     old_title = todo["title"]
+
+    # Update title with invalid title
+    todo["title"] = nil
+    put todo_path id: first_id, as: :json, params: { todo: todo }
+    assert_response :unprocessable_entity
 
     # Update title
     todo["title"] = "Another title"
@@ -71,11 +73,24 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
 
     # Retrieve same todo
-    get todo_path id: first_id
-    assert_response :success
-    todo = JSON.parse @response.body
+    todo = retrieve_single first_id
 
     # Compare titles
     assert_not_equal todo["title"], old_title
+  end
+
+  test "deleting a todo should decrease the index count" do
+    # Retrieve initial list and the first todo's ID
+    todos = retrieve_list
+    initial_count = todos.count
+    first_id = todos[0]["id"]
+
+    # Delete a todo
+    delete todo_path id: first_id
+    assert_response :no_content
+
+    # Retrieve new list
+    todos = retrieve_list
+    assert_equal todos.count, initial_count - 1
   end
 end
