58063e88488b9fcbe911d8207c87d7919f8fc6f0
Richard de Wit <henk.exe@gmail.com>
2019-02-12 00:07:04 +0100

    Let js-routes generate a custom routes file on server start

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module RaReTodos
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    JsRoutes.generate!("app/javascript/utils/routes.js", namespace: "Routes", include: /todos?/)
   end
 end
