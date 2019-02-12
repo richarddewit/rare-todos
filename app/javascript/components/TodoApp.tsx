76b38aa530b2980b0f2de927453926a395692546
Richard de Wit <henk.exe@gmail.com>
2019-02-12 02:59:04 +0100

    Refactor, scaffold and add toggle functionality

--- app/javascript/components/TodoApp.tsx
+++ app/javascript/components/TodoApp.tsx
@@ -1,15 +1,59 @@
-import React, { FunctionComponent } from "react";
+import React, {
+    FunctionComponent,
+    useMemo,
+    useState,
+} from "react";
+
+import initializeAxios from "../utils/axios";
+import * as Routes from "../utils/routes.js";
 
 import ITodo from "../interfaces/Todo";
-import TodoList from "./TodoList";
 
-const TodoApp: FunctionComponent<{ todos?: ITodo[] }> = ({ todos = [] }) => {
+import TodoListItem from "./TodoListItem";
 
-  return (
-    <>
-      <TodoList todos={todos} />
-    </>
-  );
+interface IProps {
+    todos?: ITodo[];
+    csrfToken: string;
+}
+
+const TodoApp: FunctionComponent<IProps> = ({ todos: initialTodos = [], csrfToken }) => {
+    const axios = useMemo(() => initializeAxios(csrfToken), null);
+
+    const [todos, setTodos] = useState(initialTodos);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchTodos = async () => {
+        setIsLoading(true);
+
+        const result = await axios(Routes.todos_path());
+        setTodos(result.data);
+
+        setIsLoading(false);
+    };
+
+    const toggleDone = async (todo: ITodo) => {
+        setIsLoading(true);
+
+        todo.completed_on = todo.completed_on === null ? new Date() : null;
+        await axios.put(Routes.todo_path(todo), todo);
+
+        fetchTodos();
+    };
+
+    const todoListItems = todos.map((todo: ITodo, index: number) => (
+        <TodoListItem
+            key={index}
+            todo={todo}
+            toggleDone={toggleDone}
+        />
+    ));
+
+    return (
+        <>
+            <div dangerouslySetInnerHTML={{ __html: isLoading ? "Loading..." : "&nbsp;" }} />
+            {todoListItems}
+        </>
+    );
 };
 
 export default TodoApp;
--- app/javascript/components/TodoListItem.tsx
+++ app/javascript/components/TodoListItem.tsx
@@ -1,13 +1,100 @@
-import React, { FunctionComponent } from "react";
+import dayjs from "dayjs";
+import React, { FormEvent, FunctionComponent } from "react";
 
 import ITodo from "../interfaces/Todo";
 
-const TodoListItem: FunctionComponent<{ todo: ITodo }> = ({ todo }) => {
-  const classNames = ["list-group-item"];
-  if (todo.completed_on !== null) {
-    classNames.push("list-group-item-success");
-  }
-  return <li className={classNames.join(" ")}>{todo.title}</li>;
+interface IProps {
+    todo: ITodo;
+    toggleDone: (todo: ITodo) => void;
+}
+
+const TodoListItem: FunctionComponent<IProps> = ({ todo, toggleDone }) => {
+    const isDone = todo.completed_on !== null;
+    const classNames = ["list-group-item"];
+    if (isDone) {
+        classNames.push("list-group-item-success");
+    }
+    const dueDate = todo.due_date ? dayjs(todo.due_date) : null;
+    const overdue = dueDate && dueDate.isBefore(dayjs(), "day") && !isDone;
+    const dateLabel = dueDate ? (
+        <span
+            className={"label label-" + (isDone ? "success" : (overdue ? "danger" : "default"))}
+            title={overdue ? "Overdue" : ""}
+        >
+            {dueDate.format("DD-MM-YYYY")}
+        </span>
+    ) : null;
+
+    const onToggleDone = (event: FormEvent) => {
+        event.preventDefault();
+        toggleDone(todo);
+    };
+    const onEditTodo = (event: FormEvent) => {
+        event.preventDefault();
+        // TODO: edit handler
+    };
+    const onDeleteTodo = (event: FormEvent) => {
+        event.preventDefault();
+        // TODO: delete handler
+    };
+
+    const toggleDoneForm = (
+        <form onSubmit={onToggleDone} style={{ display: "inline-block", marginRight: ".3em" }}>
+            <button
+                className={"btn btn-xs btn-" + (isDone ? "success" : "primary")}
+                type="submit"
+            >
+                <i className={"glyphicon glyphicon-" + (isDone ? "check" : "unchecked")} />
+            </button>
+        </form>
+    );
+    const editForm = isDone ? null : (
+        <form onSubmit={onEditTodo} style={{ display: "inline-block", marginRight: ".3em" }}>
+            <button
+                className="btn btn-xs btn-warning"
+                type="submit"
+            >
+                <i className="glyphicon glyphicon-pencil" />
+            </button>
+        </form>
+    );
+    const deleteForm = (
+        <form onSubmit={onDeleteTodo} style={{ display: "inline-block" }}>
+            <button
+                className="btn btn-xs btn-danger"
+                type="submit"
+            >
+                <i className="glyphicon glyphicon-trash" />
+            </button>
+        </form>
+    );
+
+    return (
+        <li className={classNames.join(" ")}>
+            <div className="row">
+                <div className="col-xs-1">
+                    {toggleDoneForm}
+                </div>
+
+                <div className="col-xs-6">
+                    <h4 className="list-group-item-heading">
+                        {todo.title}
+                    </h4>
+
+                    {todo.body && <p className="list-group-item-text">{todo.body}</p>}
+                </div>
+
+                <div className="col-xs-3 text-right">
+                    {dateLabel}
+                </div>
+
+                <div className="col-xs-2 text-right">
+                    {editForm}
+                    {deleteForm}
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default TodoListItem;
